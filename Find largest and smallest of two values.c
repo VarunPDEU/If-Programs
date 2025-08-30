6a1d0e65bf034030212778ceb112b109ca6ffbc9
Varun25BCP269 <malivarun2007@gmail.com>
2025-08-30 12:18:15 +0530

    Add files via upload

---
+++ Find largest and smallest of two values.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter Value Of a:");
+    scanf("%d",&a);
+    printf("Enter Value Of b:");
+    scanf("%d",&b);
+    if(a>b)
+    printf("a is Largest Value\n");
+    else
+    printf("b is Largest Value\n");
+    if(a<b)
+    printf("a is Smallest Value\n");
+    else
+    printf("b is Smallest Value\n");
+}
