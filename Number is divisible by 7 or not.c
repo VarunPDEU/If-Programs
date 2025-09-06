46f3211718e857d7435daebc99cef00fc8971754
Varun25BCP269 <malivarun2007@gmail.com>
2025-09-06 12:52:28 +0530

    Add files via upload

---
+++ Number is divisible by 7 or not.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	printf("Enter Value Of a:");
+	scanf("%d",&a);
+	if(a/7)
+	printf("a is divisible by 7");
+	else
+	printf("a is not divisible by 7");
+}
